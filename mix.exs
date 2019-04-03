23dee2886378ca21d161a3064942e7f136d0237a
YeJun Su <goofan.su@gmail.com>
2019-04-03 14:06:03 +0800

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule UeberauthWeixin.MixProject do
   def project do
     [
       app: :ueberauth_weixin,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
